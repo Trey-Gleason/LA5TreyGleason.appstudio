882992221c619a878fb48525083a32b032264470
Trey-Gleason <wfg15722@creighton.edu>
2021-03-03 15:11:03 -0600

    Update Strings.js

--- forms/Strings/Strings.js
+++ forms/Strings/Strings.js
@@ -1,13 +1,12 @@
 let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
-console.log(`Upper case string is: ${quoteString.toUpperCase()}`)
+alert(`Upper case string is: ${quoteString.toUpperCase()}`)
 
-let authorString = "-Henry Ford"
-let completeString = quoteString.concat(authorString)
-console.log(completeString)
+let authorString = "- Henry Ford"
+alert(authorString)
+
+let completeString = quoteString.concat('\n', authorString)
+alert(completeString)
 
 let found = completeString.includes("Henry")
+found ? alert("Henry was in completeString") : alert("Henry was NOT in completeString")
 
-  if (found == False)
-  console.log('Henry was not in completeString')
-else
-  console.log('Henry was in completeString')
